56587b2a6fb3542ef72b174db181c4147b17604c
Moatasem <mal-guna@student.42abudhabi.ae>
2022-10-06 02:10:23 +0000

    Clean

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
-#include<fstream>
-#include<sstream>
-#include<string>
+# include<iostream>
+# include<fstream>
+# include<sstream>
+# include<string>
+# include<cstdlib>
+
 void replaceContent(std::string &content, std::string s1, std::string s2)
 {
 	int index;
@@ -18,22 +20,27 @@ void replaceContent(std::string &content, std::string s1, std::string s2)
 	}
 }
 
-std::string parsefile(std::string filename)
+std::string parseFile(std::string filename)
 {
 
 	std::ifstream infile;
 	std::string line;
 	std::string content;
+
 	infile.open(filename.c_str());
 
-	if(infile)
+	if (infile.fail())
 	{
-		std::ostringstream ss;
-		ss << infile.rdbuf();
-		content = ss.str();
+		std::cerr << "Error While Opening File" << std::endl;
+		exit(EXIT_FAILURE);
 	}
 
+	std::ostringstream ss;
+	ss << infile.rdbuf();
+	content = ss.str();
+
 	infile.close();
+	
 	return (content);
 }
 
@@ -44,25 +51,25 @@ void printToFile(std::string filename, std::string content)
 	outfile.close();
 }
 
-int  prnNext(std::string errorMsg)
+int  prnError(std::string errorMsg)
 {
-	std::cout << errorMsg << std::endl;
+	std::cerr << errorMsg << std::endl;
 	return(1);
 }
 
 int main(int argc, char **argv)
 {
 	if(argc != 4)
-		return(prnNext("Wrong Number Of Args !!"));
+		return(prnError("Wrong Number Of Args !!"));
 
 	std::string filename(argv[1]);
 	std::string s1(argv[2]);
 	std::string s2(argv[3]);
 
 	if(!s1.size() || !s2.size())
-		return(prnNext("Wrong Args !!"));
+		return(prnError("Wrong Args !!"));
 
-	std::string content = parsefile(filename);
+	std::string content = parseFile(filename);
 	replaceContent(content, s1, s2);
 	printToFile(filename, content);
 
